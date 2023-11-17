b98ff268a11df50cfba6009f9e1441578f4cd9c5
Abdul Hannan Danish <mrahdanish@gmail.com>
2023-11-17 15:10:41 +0500

    Add Header Section for Instagram Clone

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,30 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://yt3.googleusercontent.com/ytc/APkrFKaqca-xQcJtp1Pqv-APucCa0nToHYGPVT00YBGOSw=s900-c-k-c0x00ffffff-no-rj" alt="Main Logo" style="height: 200px" class="rounded-circle"/>
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Let's Connect Pakistan</h1></div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>594</strong>posts</div>
+                <div class="pe-5"><strong>152k</strong>followers</div>
+                <div class="pe-5"><strong>425</strong>following</div>
+            </div>
+            <div class="pt-4 fw-bold">
+                freeCodeCamp.org
+            </div>
+            <div>
+                We're a global community of millions of people learning to code together.
+                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/
+            </div>
+            <div class="fw-bold">
+                <a href="#">www.freecodecamp.org</a>
             </div>
         </div>
+
     </div>
+
 </div>
 @endsection
